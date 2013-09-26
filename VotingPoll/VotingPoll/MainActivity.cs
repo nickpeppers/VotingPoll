4bdb08e94ec23269db5785a5b888fa5ffef6d038
Nick Peppers <nick.peppers@hitcents.com>
2013-09-26 14:10:17 -0500

    Started adding azure

--- VotingPoll/VotingPoll/MainActivity.cs
+++ VotingPoll/VotingPoll/MainActivity.cs
@@ -5,12 +5,19 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace VotingPoll
 {
 	[Activity (Label = "VotingPoll", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
+
+		public static MobileServiceClient MobileService = new MobileServiceClient
+		(
+			"https://csintermediateproject.azure-mobile.net/",
+			"zeBmdSnLPMjmgYyBgucUibIGUkwGER72"
+		);
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -21,30 +28,9 @@ namespace VotingPoll
 			var button1 = FindViewById<Button> (Resource.Id.myButton);
             var button2 = FindViewById<Button> (Resource.Id.button1);
             var text = FindViewById<TextView>(Resource.Id.textView1);
-
-            Candidate candidate1 = new Candidate { FirstName = "Bill", LastName =  "Clinton", Votes = 0 };
-            Candidate candidate2 = new Candidate { FirstName = "Chuck", LastName =  "Norris", Votes = 0 };
-            string dbLocation = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Resources);
-
-            using (var conn= new SQLite.SQLiteConnection(dbLocation))
-            {
-                conn.CreateTable<Candidate>();
-            }
-
-			button1.Click += (sender, e) =>
-            {
-                using (var db = new SQLite.SQLiteConnection(dbLocation ))
-                {
-                    db.Insert(candidate1);
-                    db.Insert(candidate2);
-                }
-			};
-
-            button2.Click += (sender, e) => 
-            {
-
-            };
 		}
+
+
 	}
 }
 
