860ba3c96936d390788df780de745bf3fc878942
Nick Peppers <nick.peppers@hitcents.com>
2013-10-15 10:29:17 -0500

    Fixed some appearance issues

--- VotingPoll/VotingPoll/Resources/Resource.designer.cs
+++ VotingPoll/VotingPoll/Resources/Resource.designer.cs
@@ -60,107 +60,104 @@ namespace VotingPoll
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05001c
-			public const int ButtonLayout = 2131034140;
-			
-			// aapt resource value: 0x7f050011
-			public const int Choice1 = 2131034129;
-			
-			// aapt resource value: 0x7f050012
-			public const int Choice1EditText = 2131034130;
+			// aapt resource value: 0x7f05001b
+			public const int ButtonLayout = 2131034139;
 			
 			// aapt resource value: 0x7f050010
-			public const int Choice1Layout = 2131034128;
+			public const int Choice1 = 2131034128;
 			
-			// aapt resource value: 0x7f050014
-			public const int Choice2 = 2131034132;
+			// aapt resource value: 0x7f050011
+			public const int Choice1EditText = 2131034129;
 			
-			// aapt resource value: 0x7f050015
-			public const int Choice2EditText = 2131034133;
+			// aapt resource value: 0x7f05000f
+			public const int Choice1Layout = 2131034127;
 			
 			// aapt resource value: 0x7f050013
-			public const int Choice2Layout = 2131034131;
+			public const int Choice2 = 2131034131;
 			
-			// aapt resource value: 0x7f050017
-			public const int Choice3 = 2131034135;
+			// aapt resource value: 0x7f050014
+			public const int Choice2EditText = 2131034132;
 			
-			// aapt resource value: 0x7f050018
-			public const int Choice3EditText = 2131034136;
+			// aapt resource value: 0x7f050012
+			public const int Choice2Layout = 2131034130;
 			
 			// aapt resource value: 0x7f050016
-			public const int Choice3Layout = 2131034134;
+			public const int Choice3 = 2131034134;
 			
-			// aapt resource value: 0x7f05001a
-			public const int Choice4 = 2131034138;
+			// aapt resource value: 0x7f050017
+			public const int Choice3EditText = 2131034135;
 			
-			// aapt resource value: 0x7f05001b
-			public const int Choice4EditText = 2131034139;
+			// aapt resource value: 0x7f050015
+			public const int Choice3Layout = 2131034133;
 			
 			// aapt resource value: 0x7f050019
-			public const int Choice4Layout = 2131034137;
+			public const int Choice4 = 2131034137;
 			
-			// aapt resource value: 0x7f050004
-			public const int ChoicesEditText = 2131034116;
+			// aapt resource value: 0x7f05001a
+			public const int Choice4EditText = 2131034138;
+			
+			// aapt resource value: 0x7f050018
+			public const int Choice4Layout = 2131034136;
 			
 			// aapt resource value: 0x7f050003
-			public const int ChoicesLayout = 2131034115;
+			public const int ChoicesEditText = 2131034115;
 			
-			// aapt resource value: 0x7f050005
-			public const int CreateChoicesButton = 2131034117;
+			// aapt resource value: 0x7f050004
+			public const int CreateChoicesButton = 2131034116;
 			
-			// aapt resource value: 0x7f050020
-			public const int CreatePollButton = 2131034144;
+			// aapt resource value: 0x7f05001f
+			public const int CreatePollButton = 2131034143;
+			
+			// aapt resource value: 0x7f05000c
+			public const int CreateResponsesCreatePollButton = 2131034124;
 			
 			// aapt resource value: 0x7f05000d
-			public const int CreateResponsesCreatePollButton = 2131034125;
-			
-			// aapt resource value: 0x7f05000e
-			public const int CurrentPollQuestion = 2131034126;
+			public const int CurrentPollQuestion = 2131034125;
 			
 			// aapt resource value: 0x7f050002
 			public const int NumberText = 2131034114;
 			
-			// aapt resource value: 0x7f050021
-			public const int PollListView = 2131034145;
+			// aapt resource value: 0x7f050020
+			public const int PollListView = 2131034144;
 			
 			// aapt resource value: 0x7f050001
 			public const int QuestionEditText = 2131034113;
 			
-			// aapt resource value: 0x7f050006
-			public const int QuestionText = 2131034118;
-			
-			// aapt resource value: 0x7f050009
-			public const int Response1 = 2131034121;
-			
-			// aapt resource value: 0x7f05000a
-			public const int Response2 = 2131034122;
-			
-			// aapt resource value: 0x7f05000b
-			public const int Response3 = 2131034123;
-			
-			// aapt resource value: 0x7f05000c
-			public const int Response4 = 2131034124;
+			// aapt resource value: 0x7f050005
+			public const int QuestionText = 2131034117;
 			
 			// aapt resource value: 0x7f050008
-			public const int ResponsesLinearLayout = 2131034120;
+			public const int Response1 = 2131034120;
 			
-			// aapt resource value: 0x7f05001e
-			public const int SubmitResponsesButton = 2131034142;
+			// aapt resource value: 0x7f050009
+			public const int Response2 = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int Response3 = 2131034122;
+			
+			// aapt resource value: 0x7f05000b
+			public const int Response4 = 2131034123;
 			
 			// aapt resource value: 0x7f050007
-			public const int Title = 2131034119;
+			public const int ResponsesLinearLayout = 2131034119;
+			
+			// aapt resource value: 0x7f05001d
+			public const int SubmitResponsesButton = 2131034141;
+			
+			// aapt resource value: 0x7f050006
+			public const int Title = 2131034118;
 			
 			// aapt resource value: 0x7f050000
 			public const int TitleText = 2131034112;
 			
-			// aapt resource value: 0x7f05001d
-			public const int ViewChartButton = 2131034141;
+			// aapt resource value: 0x7f05001c
+			public const int ViewChartButton = 2131034140;
 			
-			// aapt resource value: 0x7f05001f
-			public const int ViewPollsButton = 2131034143;
+			// aapt resource value: 0x7f05001e
+			public const int ViewPollsButton = 2131034142;
 			
-			// aapt resource value: 0x7f05000f
-			public const int textView2 = 2131034127;
+			// aapt resource value: 0x7f05000e
+			public const int textView2 = 2131034126;
 			
 			static Id()
 			{
